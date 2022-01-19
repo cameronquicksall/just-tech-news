3a5f30fad6ca17d7760636f0bae0248c210651fa
Cameron Quicksall <cameron.quicksall70@gmail.com>
2022-01-19 13:56:40 -0600

    created homepage and login method

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -60,7 +60,15 @@ const { User, Post, Comment, Vote } = require('../../models');
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
+        .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+    
+            res.json(dbUserData);
+        });
+        })
         .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -86,8 +94,25 @@ const { User, Post, Comment, Vote } = require('../../models');
         return;
         }
 
+        req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
         res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
     });
+    });
+
+    router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+        res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
     });
 
     router.put('/:id', (req, res) => {
@@ -101,7 +126,7 @@ const { User, Post, Comment, Vote } = require('../../models');
         }
     })
         .then(dbUserData => {
-        if (!dbUserData[0]) {
+        if (!dbUserData) {
             res.status(404).json({ message: 'No user found with this id' });
             return;
         }
